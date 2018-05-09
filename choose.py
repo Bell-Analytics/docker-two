2b5e365e5f4aacc1c8e811727dde23e005a1b35e
zw24code <37883105+zw24code@users.noreply.github.com>
2018-05-09 14:26:41 -0400

    Add files via upload

---
+++ choose.py
@@ -0,0 +1,8 @@
+
+
+from random import randint
+
+def choo(a):
+	b = randint(0, len(a) - 1)
+	c = a[b]
+	return(c)
---
+++ list_animal.py
@@ -0,0 +1,6 @@
+
+
+from choose import choo
+
+list_a = ["dog", "cat", "chicken"]
+print(choose(list_a))
